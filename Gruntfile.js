fbbc549252234270c98245971f4b449834df9a00
Tanjo, Hiroyuki <expheno@gmail.com>
2015-01-16 15:07:42 +0900

    Clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,13 @@
-/*
- * grunt-bower-browser
- * https://github.com/rakuten-frontend/grunt-bower-browser
- *
- * Copyright (c) 2015 Rakuten, Inc.
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 module.exports = function (grunt) {
-  // load all npm grunt tasks
+
+  // Load all npm grunt tasks.
   require('load-grunt-tasks')(grunt);
 
   // Project configuration.
   grunt.initConfig({
+
     jshint: {
       all: [
         'Gruntfile.js',
@@ -26,34 +20,15 @@ module.exports = function (grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
     // Configuration to be run (and then tested).
-    bower_browser: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
+    'bower-browser': {
+      options: {
       },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
     },
 
     // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js']
+      tests: ['test/*-test.js']
     }
 
   });
@@ -61,9 +36,9 @@ module.exports = function (grunt) {
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'bower_browser', 'nodeunit']);
+  // Whenever the "test" task is run,
+  // first run this plugin's task(s), then test the result.
+  grunt.registerTask('test', ['bower-browser', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
---
+++ tasks/bower-browser.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.registerMultiTask('bower-browser', 'Browser-based GUI manager of Bower', function () {
+  });
+
+};
